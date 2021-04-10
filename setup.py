8490a4eb7de73148dfa363983e4560d4183f73a8
Francisco Braulio <f.braulio@samsung.com>
2021-04-10 13:12:19 -0300

    commit project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analysis of emotions/sentiments from general public to news related to COVID-19 on Twitter.',
+    author='UGPN',
+    license='MIT',
+)
